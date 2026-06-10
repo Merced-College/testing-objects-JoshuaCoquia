b7a06e5c3817a8460f0305a48229988636ad5152
Joshua Coquia <mail@joshuacoquia.com>
2026-06-09 20:03:47 -0700

    feat: add 3 fields to Course object

--- Course.java
+++ Course.java
@@ -1,5 +1,10 @@
 public class Course {
-	// TODO: Create 3 Fields w/ default values
+	// This class represents a university course from the perspective of a student.
+	private String name;
+	private double overallGradePercentage;
+	private int assignmentCount; // Assignments could be its own object if I wanted to...
+	// I decided not to implement a separate Assignments object for simplicity.
+
 	// TODO: Add a default and parameterized constructor
 	// TODO: Print this object using `toString` before using @override
 	// TODO: @override `toString()` and reprint the object

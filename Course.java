48868d020130e7d5c3d6e1c385149f7bcc7a95ad
Joshua Coquia <mail@joshuacoquia.com>
2026-06-09 20:06:01 -0700

    feat: add default constructor/no-arg constructor for Course class

--- Course.java
+++ Course.java
@@ -5,7 +5,13 @@ public class Course {
 	private int assignmentCount; // Assignments could be its own object if I wanted to...
 	// I decided not to implement a separate Assignments object for simplicity.
 
-	// TODO: Add a default and parameterized constructor
+	public Course() {
+		this.name = "Course";
+		this.overallGradePercentage = 0; // Assuming that a student starts with a grade of 0.
+		this.assignmentCount = 0;
+	}
+
+	// TODO: Add a parameterized constructor
 	// TODO: Print this object using `toString` before using @override
 	// TODO: @override `toString()` and reprint the object
 	// TODO: Implement encapsulation and explain validation rule

695fab61a1b42ff0809485e2d6fecd67503f0e7a
Joshua Coquia <mail@joshuacoquia.com>
2026-06-09 18:41:27 -0700

    Step 2

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		// A1: Create a Dog with no constructors defined.
+		Dog dog1 = new Dog();
+
+		// A2: Print the object directly.
+		System.out.println("Printing dog1 directly:");
+		System.out.println(dog1);
+	}
+}
